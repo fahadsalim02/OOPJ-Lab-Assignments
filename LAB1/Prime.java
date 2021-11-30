6c1c8bb34c33da294149734c843deaf1a296f223
Fahad Salim <85647102+fahadsalim02@users.noreply.github.com>
2021-11-30 07:04:00 +0530

    Create Prime.java

---
+++ LAB1/Prime.java
@@ -0,0 +1,26 @@
+package LAB1;
+import java.util.*;
+public class PrimeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int num=sc.nextInt();
+		    boolean flag = false;
+		    for (int i = 2; i <= num / 2; ++i) {
+		      // condition for nonprime number
+		      if (num % i == 0) {
+		        flag = true;
+		        break;
+		      }
+		    }
+
+		    if (!flag)
+		      System.out.println(num + " is a prime number.");
+		    else
+		      System.out.println(num + " is not a prime number.");
+
+}
+	
+}
